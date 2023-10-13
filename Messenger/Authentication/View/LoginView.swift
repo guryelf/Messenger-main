05f09c6a0eef402c8050767e132b08e20e73e2be
FG <furkangryl917@gmail.com>
2023-10-13 03:01:55 +0300

    small changes

--- Messenger/Authentication/View/LoginView.swift
+++ Messenger/Authentication/View/LoginView.swift
@@ -34,11 +34,6 @@ struct LoginView: View {
                     .background(Color(uiColor: .systemGray6))
                     .cornerRadius(20)
                     .padding(.horizontal,20)
-                Text(isPasswordEmpty ? "This field cannot be empty." : "")
-                    .foregroundStyle(Color(.systemRed))
-                    .padding(.trailing, 150)
-                    .font(.system(size: 15))
-                    .frame(height: 0)
             }
             VStack(spacing: 20){
                 Button(action:{
@@ -87,15 +82,6 @@ struct LoginView: View {
     }
 }
 
-extension LoginView: LoginValidation{
-    var isEmailEmpty: Bool {
-        return viewModel.email.isEmpty
-    }
-    
-    var isPasswordEmpty: Bool {
-        return viewModel.password.isEmpty
-    }
-}
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
